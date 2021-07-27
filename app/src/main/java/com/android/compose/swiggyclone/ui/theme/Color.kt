8ff5403ed47118c795bb759ef676ad6c97765592
Pradeep <lspradeep21@gmail.com>
2021-07-27 23:23:29 +0530

    second commit

--- app/src/main/java/com/android/compose/swiggyclone/ui/theme/Color.kt
+++ app/src/main/java/com/android/compose/swiggyclone/ui/theme/Color.kt
@@ -11,3 +11,11 @@ val greyLight = Color(0xFFF3F3F3)
 val grey = Color(0xFFE4DBDB)
 val greyDark = Color(0xFF5A5858)
 val white = Color(0xFFFFFFFF)
+val brown = Color(0xFF972F28)
+
+val pinkGradiant = listOf(
+    Color(0xFFC97D65),
+    Color(0xFFDF7553),
+    Color(0xFFB9607E),
+    Color(0xFFB9426A),
+)
--- app/src/main/java/com/android/compose/swiggyclone/widgets/OfferBannerTypeOne.kt
+++ app/src/main/java/com/android/compose/swiggyclone/widgets/OfferBannerTypeOne.kt
@@ -1,34 +1,79 @@
 package com.android.compose.swiggyclone.widgets
 
-import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.ArrowRight
+import androidx.compose.material.icons.rounded.NavigateNext
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import com.android.compose.swiggyclone.ui.theme.greyLight
+import com.android.compose.swiggyclone.ui.theme.*
+import com.android.compose.swiggyclone.R
 
 @Composable
 fun OfferBannerTypeOne(modifier: Modifier) {
     Box(modifier.padding(8.dp)) {
-        Box(
+        Row(
             modifier
                 .fillMaxWidth()
                 .height(180.dp)
-                .clip(shape = RoundedCornerShape(8.dp))
-                .background(color = Color.Blue)
+                .clip(shape = RoundedCornerShape(16.dp))
+                .background(brush = Brush.horizontalGradient(colors = pinkGradiant), alpha = 0.6f)
 
         ) {
+            Column(
+                horizontalAlignment = Alignment.Start,
+                verticalArrangement = Arrangement.Center,
+                modifier = modifier
+                    .padding(16.dp)
+                    .fillMaxHeight()
+                    .weight(0.6f)
+            ) {
+                Text(
+                    text = "up to 60% off try new tastes".uppercase(),
+                    style = Typography.h6.copy(
+                        color = brown,
+                        fontWeight = FontWeight.Bold
+                    )
+                )
 
+                Text(
+                    text = "Fresh offers to enjoy\nnew dishes everyday.",
+                    style = Typography.subtitle2.copy(
+                        color = brown,
+                    ),
+                    modifier = modifier.padding(top = 8.dp, bottom = 8.dp)
+                )
+
+                Image(
+                    imageVector = Icons.Rounded.NavigateNext,
+                    contentDescription = "",
+                    modifier = modifier
+                        .background(shape = RoundedCornerShape(8.dp), color = white)
+                        .padding(4.dp),
+                )
+            }
+
+            Image(
+                painter = painterResource(id = R.drawable.burger),
+                contentDescription = "",
+                modifier = modifier
+                    .align(Alignment.CenterVertically)
+                    .weight(0.4f)
+                    .fillMaxHeight(),
+            )
         }
     }
 }
